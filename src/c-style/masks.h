af4b91f849326faecdefe4fe4f65705d20056a38
oceanborn-mx <da.razo@yahoo.com>
2015-01-16 22:34:38 -0600

    Added masks.h header

---
+++ src/c-style/masks.h
@@ -0,0 +1,11 @@
+// 5x5 shapes
+// MSHAPE_CROSS   MSHAPE_DIAMOND    MSHAPE_OCTO    MSHAPE_FULL
+//        00100            00100          01110          11111
+//        00100            01110          11111          11111
+//        11111            11111          11111          11111
+//        00100            01110          11111          11111
+//        00100            00100          01110          11111
+
+// TODO: add mask shapes with the size 5x5
+
+
