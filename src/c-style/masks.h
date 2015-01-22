cffdedc749a78e58609c4210c89fe89e183b8376
oceanborn-mx <da.razo@yahoo.com>
2015-01-21 21:56:15 -0600

    masks defined

--- src/c-style/masks.h
+++ src/c-style/masks.h
@@ -6,6 +6,43 @@
 //        00100            01110          11111          11111
 //        00100            00100          01110          11111
 
-// TODO: add mask shapes with the size 5x5
+// masks 3x3
+// shaped-cross mask
+const unsigned int MASK33CROSS[3][3]   =  {{0xFF000000, 0xFFFFFFFF, 0xFF000000},
+				           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+				           {0xFF000000, 0xFFFFFFFF, 0xFF000000}};                                     
+				           
 
+// shaped-full mask
+const unsigned int MASK33FULL[3][3]    =  {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+                                           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+                                           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}};
 
+// masks 5x5
+// shaped-cross mask
+const unsigned int MASK55CROSS[5][5]   =  {{0xFF000000, 0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFF000000},
+				           {0xFF000000, 0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFF000000},
+				           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+                                           {0xFF000000, 0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFF000000},
+				           {0xFF000000, 0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFF000000}};
+
+// shaped-diamond mask
+const unsigned int MASK55DIAMOND[5][5] =  {{0xFF000000, 0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFF000000},
+				           {0xFF000000, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFF000000},
+				           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+                                           {0xFF000000, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFF000000},
+                                           {0xFF000000, 0xFF000000, 0xFFFFFFFF, 0xFF000000, 0xFF000000}};
+
+// shaped-octo mask
+const unsigned int MASK55OCTO[5][5]    =  {{0xFF000000, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFF000000},
+				           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+				           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+                                           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+                                           {0xFF000000, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFF000000}};
+
+// shaped-full mask
+const unsigned int MASK55FULL[5][5]    =  {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+				           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+				           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+                                           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF},
+                                           {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}};
